4a93eeadba8ec1fe3c1b65118beb8a610daf6b47
Tao <werewaswas@live.com>
2018-08-05 03:37:02 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,11 @@
 
+def back_project(project)
+  project.backers << self
+end
+
 def backed_projects
-  Project.all.select do |project|
-    project.backer == self
+  Project.all.select do |project| 
+    project.backers.include? self
   end
 end
+
