3583927ddaa83d2a50369bf5131e3f0f51085250
Tao <werewaswas@live.com>
2018-08-05 03:38:45 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,17 @@
-class Backer 
+class Backer
+
+  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-  
+
   def back_project(project)
     project.backers << self
   end
 
   def backed_projects
-    Project.all.select do |project| 
+    Project.all.select do |project|
       project.backers.include? self
     end
   end
